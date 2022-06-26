fb5be3f78639c74f9b4cb79491c7bb20f6d87394
Vinh <nguyenvinh1997@yahoo.de>
2022-06-26 19:57:06 +0200

    get user input and validate

--- app.js
+++ app.js
@@ -1,38 +1,76 @@
 const prompt = require('prompt-sync')();
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const AdBlockerPlugin = require('puppeteer-extra-plugin-adblocker');
+
+const skyscanner_url = 'https://www.skyscanner.de';
 
 const search = prompt('Press 1 for search with airport code, 2 for search with airport name: ');
+let originAirportCode = '';
+let destinationAirportCode = '';
+let originAirportName = '';
+let destinationAirportName = '';
 
 if (search == 1) {
-    const originAirportCode = prompt('Enter origin airport code: ');
-    console.log(`origin airport code: ${originAirportCode}`);
-
-    const destinationAirportCode = prompt('Enter destination airport code: ');
-    console.log(`destination airport code: ${destinationAirportCode}`);
+    originAirportCode = prompt('Enter origin airport code: ');
+    destinationAirportCode = prompt('Enter destination airport code: ');
 } else if (search == 2) {
-    const originAirportName = prompt('Enter irigin airport name: ');
-    console.log(`origin airport name: ${originAirportName}`);
-
-    const destinationAirportName = prompt('Enter destination airport name: ');
-    console.log(`destination airport name: ${destinationAirportName}`);
+    originAirportName = prompt('Enter irigin airport name: ');
+    destinationAirportName = prompt('Enter destination airport name: ');
 } else {
     console.log('God is a woman');
 }
 
-const departureDate = prompt('Enter departure date (dd.mm.yyyy): ');
-console.log(`departure date:  ${departureDate}`);
+let departureDate = prompt('Enter departure date (dd.mm.yyyy): ');
+let returnDate = prompt('Enter return date (dd.mm.yyyy): ');
 
-const returnDate = prompt('Enter return date (dd.mm.yyyy): ');
-console.log(`Return date: ${returnDate}`)
+if (checkDate(departureDate) && checkDate(returnDate) && returnDate > departureDate) {
+    if (search == 1) {
+        console.log('Origin: ' + originAirportCode + ' Destination: ' + destinationAirportCode + '\nDeparture: ' + departureDate + ' Return: ' + returnDate);
+    } else if (search == 2) {
+        console.log('Origin: ' + originAirportName + ' Destination: ' + destinationAirportName + '\nDeparture: ' + departureDate + ' Return: ' + returnDate);
+    }
 
+    departureDate = formatDate(departureDate);
+    returnDate = formatDate(returnDate);
+} else {
+    console.log('Invalid date');
+    process.exit();
+}
 
-const skyscanner_url = 'https://www.skyscanner.de';
+function checkDate(date) {
+    if (date.length != 10) {
+        return false;
+    }
 
-const puppeteer = require('puppeteer-extra');
+    const dateArray = date.split('.');
+    const day = dateArray[0];
+    const month = dateArray[1];
+    const year = dateArray[2];
 
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+    if (day < 1 || day > 31 || day === undefined) {
+        return false;
+    }
+    if (month < 1 || month > 12 || month === undefined) {
+        return false;
+    }
+    if (year < 0 || year > 9999 || year === undefined) {
+        return false;
+    }
+    return true;
+}
+
+function formatDate(date) {
+    const dateArray = date.split('.');
+    const day = dateArray[0];
+    const month = dateArray[1];
+    const year = dateArray[2].slice(-2);
+
+    return `${year}${month}${day}`;
+}
+
 puppeteer.use(StealthPlugin());
 
-const AdBlockerPlugin = require('puppeteer-extra-plugin-adblocker');
 puppeteer.use(AdBlockerPlugin({ blockTrackers: true }));
 
 puppeteer.launch({ headless: true }).then(async browser => {
